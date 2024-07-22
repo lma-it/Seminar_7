64aef7ad1c004a984339d4766161ce2433d29809
Михаил Лубенцов <wispergently061@gmail.com>
2024-07-22 13:01:47 +0500

    Разобрали ключевые паттерны проектирования - Builder, Adapter, Strategy, Decorator.

---
+++ oop_task1/adapter/Converter.java
@@ -0,0 +1,6 @@
+package oop_task1.adapter;
+import java.util.List;
+
+public interface Converter {
+    String convert(List<String> keys, List<String> values);
+}
---
+++ oop_task1/adapter/XMLConverter.java
@@ -0,0 +1,13 @@
+package oop_task1.adapter;
+
+import java.util.List;
+
+public class XMLConverter implements Converter{
+
+    @Override
+    public String convert(List<String> keys, List<String> values) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convert'");
+    }
+    
+}
